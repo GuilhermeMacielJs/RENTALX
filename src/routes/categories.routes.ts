a3a907a9cb194be12a2b04e4b3a4cede5e9db472
Guilherme Maciel <67983309+GuilhermeMacielJs@users.noreply.github.com>
2021-11-01 09:34:00 -0300

    Introdução ao upload de arquivo com Multer e introdução a documentação de api com swagger.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { importCategoryController } from "../modules/cars/useCases/importCategory/index";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
+const upload = multer({
+    dest: "./tmp",
+});
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
@@ -11,6 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
