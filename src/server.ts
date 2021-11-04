f20e6f9776442c32995d167c18fa639129c50bf1
Guilherme Maciel <67983309+GuilhermeMacielJs@users.noreply.github.com>
2021-11-04 17:04:15 -0300

    Introdução ao docker e docker composer.

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,10 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
+
+app.get("/", (request, response) => {
+    return response.json({ name: "Teste" });
+});
 
 app.listen(3333, () => {
     console.log("aplicação rodando");
